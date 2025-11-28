ee919b3f230342693c6ef12ceaf31d8df4222d8e
Ubuntu <ubuntu@ip-172-31-47-42.ap-south-1.compute.internal>
2025-11-28 08:45:53 +0000

    one

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 set -e
-IMAGE=$1
 
+# ---- Hardcoded Docker Hub Credentials ----
+DOCKER_USER="robbenselvaone"
+DOCKER_PASS="dckr_pat_BCuPbALPEfSZQzzAdD92xO4xHX4"
+REGISTRY="docker.io"
+# ------------------------------------------
+
+IMAGE=$1
 export IMAGE=$IMAGE
+
+echo "Logging in to Docker Hub..."
+echo "$DOCKER_PASS" | docker login "$REGISTRY" -u "$DOCKER_USER" --password-stdin
+
+echo "Pulling image: $IMAGE"
 docker compose pull
+
+echo "Starting containers..."
 docker compose up -d
+
+echo "Deployment completed successfully!"
+
